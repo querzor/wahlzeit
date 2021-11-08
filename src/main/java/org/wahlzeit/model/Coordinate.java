9822c8e8e10dd1dda2a95946f8bad22a0432ec6d
Simon Wiesend <simon.wiesend@fau.de>
2021-11-08 12:02:26 +0100

    adap-cw02

--- src/main/java/org/wahlzeit/model/Coordinate.java
+++ src/main/java/org/wahlzeit/model/Coordinate.java
@@ -1,12 +1,25 @@
 package org.wahlzeit.model;
 
 public class Coordinate {
-    double x, y, z;
+    private double x, y, z;
 
     public Coordinate(double x, double y, double z) {
         this.x = x;
         this.y = y;
         this.z = z;
+    }
+
+    //returns the Euclidean distance to the given coordinate
+    public double getDistance(Coordinate coordinate) {
+        return Math.sqrt(Math.pow(this.x - coordinate.getX(), 2) + Math.pow(this.y - coordinate.getY(), 2) + Math.pow(this.z - coordinate.getZ(), 2));
+    }
+
+    public boolean equals(Coordinate coordinate) {
+        return isEqual(coordinate);
+    }
+
+    public boolean isEqual(Coordinate coordinate) {
+        return coordinate.getX() == this.x && coordinate.getY() == this.y && coordinate.getZ() == this.z;
     }
 
     public double getX() {
